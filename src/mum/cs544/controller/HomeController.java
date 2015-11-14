6c968c9fc107fdf84f12a821b7f8fa18982f427c
jgsadang <jgsadang3@gmail.com>
2015-11-15 02:48:41 +0800

    v2

--- src/mum/cs544/controller/HomeController.java
+++ src/mum/cs544/controller/HomeController.java
@@ -1,10 +1,15 @@
 package mum.cs544.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import mum.cs544.serviceImpl.InstructorServiceImpl;
+
 @Controller
 public class HomeController {
+	@Autowired
+	private InstructorServiceImpl instructorServiceImpl;
 	
 	@RequestMapping("/")
 	public String showMenu() {
